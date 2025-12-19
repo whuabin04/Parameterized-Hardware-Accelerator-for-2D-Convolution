d5c0c6be4273ffd03f09beedf194b5c767e99738
Hua Bin Wu <133434193+whuabin04@users.noreply.github.com>
2025-12-19 18:09:38 -0500

    Delete 507 directory

--- 507/Part1/params.sv
+++
@@ -1,3 +0,0 @@
-`define INWVAL 12
-`define OUTWVAL 24
-`define PIPELINEDVAL 1
--- 507/Part5/params.sv
+++
@@ -1,6 +0,0 @@
-`define INWVAL 24
-`define RVAL 16
-`define CVAL 17
-`define MAXKVAL 9
-`define TVPR 1.0
-`define TRPR 1.0
